a3b60fde460cb1117d2b88661a3051ca54304cab
Eduardo Murillo <eduardo.murillo@jemmic.com>
2026-01-04 21:40:41 +0100

    chore: add APK build script, gh-pages deploy, and twa-manifest

---
+++ app/src/main/java/com/emg1990/restaurantpwa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.emg1990.restaurantpwa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
